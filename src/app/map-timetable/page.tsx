6213fce14c453161e49f08170ed67a409cbbc877
seongmin <isah1609@naver.com>
2025-05-31 00:11:01 +0900

    에러수정

--- src/app/map-timetable/page.tsx
+++ src/app/map-timetable/page.tsx
@@ -1,4 +1,9 @@
-import MapTimetable from '../../../components/map-timetable';
+// app/map-timetable/page.tsx
+import dynamic from 'next/dynamic';
+
+const MapTimetable = dynamic(() => import('../../..//components/map-timetable'), {
+  ssr: false, // ❗ 서버에서 렌더링 안 하게 설정
+});
 
 export default function MapTimetablePage() {
   return <MapTimetable />;
